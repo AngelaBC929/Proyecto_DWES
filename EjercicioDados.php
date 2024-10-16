183980fe62f48a6bc238b705ea8b233c7947e07d
Angela <98353522+AngelaBC929@users.noreply.github.com>
2024-10-16 23:59:23 +0200

    Subo el html para empezar el proyecto

---
+++ EjercicioDados.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio Dados</title>
+</head>
+
+<body>
+    <h1>Ejercicio 1</h1>
+    <div class="ejercicio dados">
+        <h2>¿Cuantos dados quieres lanzar? (1-10):</h2>
+        <input type="number" name="numero" required />
+        <button type="submit">Lanzar</button>
+    </div>
+</body>
+
+</html>
