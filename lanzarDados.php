99dc22865f3036fabd8b3d8821b4935a8b2601cf
marcosvarela94 <145340936+marcosvarela94@users.noreply.github.com>
2024-10-17 10:02:49 +0200

    Completo php lanzarDados.php

--- lanzarDados.php
+++ lanzarDados.php
@@ -1,4 +1,4 @@
-    <?php
+<?php
     $numero = $_REQUEST["numero"];
 
     // Mostrar el número de dados
@@ -8,3 +8,30 @@
         echo '<h2>' . $numero . ' dado</h2>';
     }
     ?>
+
+    <div class="dados">
+        <?php
+        $img = ['img/1.svg', 'img/2.svg', 'img/3.svg', 'img/4.svg', 'img/5.svg', 'img/6.svg'];
+        $impares = 0;
+        $pares = 0;
+
+        // Comprobar si el número es válido
+        if (is_numeric($numero) && ($numero >= 1 && $numero <= 10)) {
+            for ($i = 0; $i < $numero; $i++) {
+                $numAleatorio = rand(0, count($img) - 1);
+                echo '<img src="' . $img[$numAleatorio] . '">';
+
+                // Extraemos el número de la imagen (1 a 6)
+                $numeroDado = $numAleatorio + 1; // +1 porque el índice empieza en 0
+        
+                // Comprobamos si es par o impar
+                if ($numeroDado % 2 != 0) {
+                    $impares++;
+                } else {
+                    $pares++;
+                }
+            }
+        } else {
+            print "<div class='resultado-texto'>Número inválido</div>";
+        }
+        ?>
