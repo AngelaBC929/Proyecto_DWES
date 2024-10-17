aef0a91e82299d7744a1fe1298c9812046174fc5
marcosvarela94 <145340936+marcosvarela94@users.noreply.github.com>
2024-10-17 10:09:34 +0200

    Combino PHP, HTML y CSS de lanzarDados

--- lanzarDados.php
+++ lanzarDados.php
@@ -1,4 +1,60 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: antiquewhite;
+            margin: 0;
+            padding: 20px;
+        }
+
+        h1 {
+            text-decoration:underline 2px brown;        
+        }
+
+        h2,h1 {
+            color: brown;
+            text-align: center;
+        }
+
+        .dados {
+            background-color: #c5f9d1;
+            display: flex;
+            justify-content: center; /* Centramos horizontalmente */
+            flex-wrap: wrap;/* Ajustamos los dados a varias filas */
+            max-width: 500px;
+            margin: 10px auto;
+            padding: 20px;
+            border-radius: 5px;
+            border: 2px solid brown;
+            text-align: center;
+            border-radius: 10px;
+            box-shadow: 7px 7px 7px #138d75;
+        }
+
+        .dados img {
+            width: 100px;
+            height: 100px;
+            margin: 5px;
+        }
+
+        .resultado-texto {
+            text-align: center;
+            margin-top: 20px;
+        }
+        
+    </style>
+</head>
+
+<body>
+    <h1>Resultado</h1>
+
+    <?php
     $numero = $_REQUEST["numero"];
 
     // Mostrar el número de dados
@@ -35,3 +91,14 @@
             print "<div class='resultado-texto'>Número inválido</div>";
         }
         ?>
+    </div>
+
+    <!-- Mostrar resultados de pares e impares -->
+    <div class="resultado-texto">
+        <h3>
+            <font color=brown>Han salido <?php echo $impares; ?> números impares y <?php echo $pares; ?> números pares.</font>
+        </h3>
+    </div>
+</body>
+
+</html>
